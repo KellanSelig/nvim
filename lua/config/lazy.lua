26b63905477200e8ab551830cb036ee43c5acb9a
KellanSelig <46583276+KellanSelig@users.noreply.github.com>
2025-02-06 10:48:56 -0500

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,6 +35,9 @@ require("lazy").setup({
   {
     "tpope/vim-surround",
   },
+  {
+    "tpope/vim-repeat",
+  },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
